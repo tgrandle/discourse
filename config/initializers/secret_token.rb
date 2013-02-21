400e499cd68342512fcd0c082771299bd42e8a2c
Trevor Grandle <tgrandle@gmail.com>
2013-02-20 21:02:30 -0500

    fixing secret_token for heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -11,6 +11,14 @@
 if Rails.env.test? || Rails.env.development? 
   Discourse::Application.config.secret_token = "47f5390004bf6d25bb97083fb98e7cc133ab450ba814dd19638a78282b4ca291" 
 else 
-  raise "You must set a secret token in config/initializers/secret_token.rb"
+	# tjg for heroku production
+	if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+	  raise 'SECRET_TOKEN environment variable must be set!'
+	end	
+	Discourse::Application.config.secret_token = ENV['SECRET_TOKEN']
+
+	if Discourse::Application.config.secret_token.blank?
+  		raise "You must set a secret token in config/initializers/secret_token.rb"
+  	end
 end
 

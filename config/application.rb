d2597c4b6ad9c2391afaf555e1ca71d6cbd30ea1
Trevor Grandle <tgrandle@gmail.com>
2013-02-20 19:53:55 -0500

    removing redis cache for heroku

--- config/application.rb
+++ config/application.rb
@@ -86,7 +86,8 @@ module Discourse
     redis_config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))[Rails.env]
     redis_store = ActiveSupport::Cache::RedisStore.new "redis://#{redis_config['host']}:#{redis_config['port']}/#{redis_config['cache_db']}"
     redis_store.options[:namespace] = -> { DiscourseRedis.namespace }
-    config.cache_store = redis_store
+    #tjg disable regis cache for heroku
+    #config.cache_store = redis_store
 
     # Test with rack::cache disabled. Nginx does this for us
     config.action_dispatch.rack_cache =  nil
